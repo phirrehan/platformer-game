37400dab54bc16b5fbbb1a500fa6117338de3e6b
phirrehan <phirrehan@gmail.com>
2025-10-09 17:21:28 +0500

    renamed res to assets, added animations

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyListener;
 
 import main.GamePanel;
 
+import static utils.Constants.Directions.*;
+
 public class KeyboardInputs implements KeyListener {
 
   private GamePanel gamePanel;
@@ -20,19 +22,31 @@ public class KeyboardInputs implements KeyListener {
 
   @Override
   public void keyReleased(KeyEvent e) {
-
+    switch (e.getKeyCode()) {
+      case KeyEvent.VK_W:
+      case KeyEvent.VK_S:
+      case KeyEvent.VK_A:
+      case KeyEvent.VK_D:
+        gamePanel.setMoving(false);
+        break;
+    }
   }
 
   @Override
   public void keyPressed(KeyEvent e) {
     switch (e.getKeyCode()) {
       case KeyEvent.VK_W:
+        gamePanel.setDirection(UP);
         break;
       case KeyEvent.VK_S:
+        gamePanel.setDirection(DOWN);
         break;
       case KeyEvent.VK_A:
+        gamePanel.setDirection(LEFT);
         break;
       case KeyEvent.VK_D:
+        gamePanel.setDirection(RIGHT);
+        break;
     }
   }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -13,13 +13,23 @@ import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import static utils.Constants.PlayerConstants.*;
+import static utils.Constants.Directions.*;
+
 public class GamePanel extends JPanel {
 
   private MouseInputs mouseInputs;
-  private BufferedImage img, subImg;
+  private int xPos = 100, yPos = 100;
+  private BufferedImage img;
+  private BufferedImage[][] animations;
+  private int aniTick, aniIndex, aniSpeed = 15; // 120 / 15 = 8 animations per second
+  private int playerAction = IDLE;
+  private int playerDir = -1;
+  private boolean moving;
 
   public GamePanel() {
     importImg();
+    loadAnimations();
     mouseInputs = new MouseInputs(this);
     setPanelSize();
     addKeyListener(new KeyboardInputs(this));
@@ -27,8 +37,25 @@ public class GamePanel extends JPanel {
     addMouseMotionListener(mouseInputs);
   }
 
+  public void setXPos(int x) {
+    this.xPos = x;
+  }
+
+  public void setYPos(int y) {
+    this.yPos = y;
+  }
+
+  public void setDirection(int direction) {
+    this.playerDir = direction;
+    setMoving(true);
+  }
+
+  public void setMoving(boolean moving) {
+    this.moving = moving;
+  }
+
   private void importImg() {
-    File file = new File("../res/player_sprites.png");
+    File file = new File("../assets/player_sprites.png");
     try {
       img = ImageIO.read(file);
     } catch (IOException e) {
@@ -36,16 +63,63 @@ public class GamePanel extends JPanel {
     }
   }
 
+  private void loadAnimations() {
+    animations = new BufferedImage[9][6];
+    for (int i = 0; i < animations.length; i++)
+      for (int j = 0; j < getSpriteAmount(i); j++)
+        animations[i][j] = img.getSubimage(64 * j, 40 * i, 64, 40);
+  }
+
   private void setPanelSize() {
     Dimension size = new Dimension(1280, 800);
     setPreferredSize(size);
   }
 
+  private void updateAnimationTick() {
+    aniTick++;
+    if (aniTick == aniSpeed) {
+      aniTick = 0;
+      aniIndex++;
+      if (aniIndex >= getSpriteAmount(playerAction))
+        aniIndex = 0;
+    }
+  }
+
+  private void setAnimation() {
+    if (moving)
+      playerAction = RUNNING;
+    else
+      playerAction = IDLE;
+  }
+
+  private void updatePos() {
+    if (moving) {
+      switch (playerDir) {
+        case LEFT:
+          xPos += -5;
+          break;
+        case UP:
+          yPos += -5;
+          break;
+        case RIGHT:
+          xPos += 5;
+          break;
+        case DOWN:
+          yPos += 5;
+          break;
+      }
+    }
+  }
+
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
 
-    subImg = img.getSubimage(1 * 64, 8 * 40, 64, 40);
-    g.drawImage(subImg, 0, 0, 128, 80, null);
+    updateAnimationTick();
+
+    setAnimation();
+    updatePos();
+
+    g.drawImage(animations[playerAction][aniIndex], xPos, yPos, 64 * 4, 40 * 4, null);
   }
 
 }
